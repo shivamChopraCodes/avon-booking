30254517b774682a1c0ea8dc5eb4a43d3bd156cf
Shivam Chopra <shivamchopra@gokwik.co>
2022-12-03 23:30:10 +0530

    fix: homepage loading animation added

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,7 @@ import DealSlider from '../src/components/dealsSlider';
 import FeaturedDeals from '../src/components/featuredDeals';
 import HeroInputs from '../src/components/inputs/inputsContainer';
 import Services from '../src/components/services';
+import Spinner from '../src/components/spinner';
 import Testimonial from '../src/components/testimonial';
 import TravelCards from '../src/components/travelCards';
 import styles from '../styles/Home.module.css';
@@ -55,8 +56,10 @@ import styles from '../styles/Home.module.css';
 
 export default function Home() {
   const [data, setData] = useState({});
+  const [loading, setLoading] = useState(false);
   const [weeklySpecials, setWeeklySpecials] = useState(null);
   const fetchWeeklySpecials = async () => {
+    setLoading(true);
     const res = await fetch(`./api/homepage-data/best-tour`, {
       method: 'GET',
       headers: {
@@ -90,6 +93,7 @@ export default function Home() {
       ...best_tour,
       ...amazing_deals,
     });
+    setLoading(false);
   };
   useEffect(() => {
     fetchWeeklySpecials();
@@ -98,54 +102,64 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <main className={styles.main}>
-        <div
-          className={`${styles.headWrapper} index-header-wrapper flex flex-col justify-center lg:justify-end items-center text-white `}
-        >
-          <h1 className='flex flex-col justify-center items-center text-center lg:mt-60'>
-            <p className=' text-xl font-semibold '>The best tour experience</p>
-            <p className=' text-[28px] font-bold my-4 '>
-              find and book best <span className='text-primary-yellow'>tickets</span>
-            </p>
-          </h1>
-          <div className='w-5/6 lg:w-10/12 mt-16 overflow-hidden deals-slider'>
-            <DealSlider logos={data?.logos} data={data?.best_tour} />
-          </div>
-        </div>
-        <div
-          className='py-20 flex flex-col w-full justify-center items-center text-center lg:-mt-52 relative z-10 bg-white'
-          id={'about-us'}
-        >
-          <p
-            className='text-2xl lg:text-3xl font-bold text-primary-blue'
-            data-aos='fade-down'
-            data-aos-anchor-placement='top-bottom'
+        {loading ? (
+          <div
+            className={`${styles.headWrapper} index-header-wrapper flex flex-col justify-center items-center text-white `}
           >
-            Amazing Deals
-          </p>
-          <TravelCards logos={data?.logos} data={data?.amazing_deals} />
-        </div>
-        <div className='py-20 flex flex-col w-full justify-center items-center text-center' id={'featured-deals'}>
-          <p className='text-2xl lg:text-3xl font-bold text-primary-blue'>Weekly Specials</p>
-          <FeaturedDeals logos={data?.logos} data={data.weekly_specials} />
-        </div>
-        <div className='py-20 flex flex-col w-full justify-center items-center text-center overflow-hidden'>
-          <p className='text-2xl lg:text-3xl font-bold text-primary-blue'>Our Partners</p>
-          <div className='w-11/12 lg:w-5/6 my-8'>
-            <Carousel />
+            <Spinner />
           </div>
-        </div>
-        <div className='py-20 w-full' id='services'>
-          <Services />
-        </div>
-        <div className=' relative my-8 w-100 flex flex-col justify-center lg:justify-between items-center'>
-          <div className=' mb-4 flex flex-col justify-center items-center pt-9  text-center '>
-            <p className='text-slate-400 text-sm lg:text-lg my-3'>A WORD FROM </p>
-            <p className='text-primary-blue font-black text-2xl lg:text-4xl'>Our Customers</p>
-          </div>
-          <div className='testimonials w-100  flex flex-col items-center'>
-            <Testimonial />
-          </div>
-        </div>
+        ) : (
+          <>
+            <div
+              className={`${styles.headWrapper} index-header-wrapper flex flex-col justify-center lg:justify-end items-center text-white `}
+            >
+              <h1 className='flex flex-col justify-center items-center text-center lg:mt-60'>
+                <p className=' text-xl font-semibold '>The best tour experience</p>
+                <p className=' text-[28px] font-bold my-4 '>
+                  find and book best <span className='text-primary-yellow'>tickets</span>
+                </p>
+              </h1>
+              <div className='w-5/6 lg:w-10/12 mt-16 overflow-hidden deals-slider'>
+                <DealSlider logos={data?.logos} data={data?.best_tour} />
+              </div>
+            </div>
+            <div
+              className='py-20 flex flex-col w-full justify-center items-center text-center lg:-mt-52 relative z-10 bg-white'
+              id={'about-us'}
+            >
+              <p
+                className='text-2xl lg:text-3xl font-bold text-primary-blue'
+                data-aos='fade-down'
+                data-aos-anchor-placement='top-bottom'
+              >
+                Amazing Deals
+              </p>
+              <TravelCards logos={data?.logos} data={data?.amazing_deals} />
+            </div>
+            <div className='py-20 flex flex-col w-full justify-center items-center text-center' id={'featured-deals'}>
+              <p className='text-2xl lg:text-3xl font-bold text-primary-blue'>Weekly Specials</p>
+              <FeaturedDeals logos={data?.logos} data={data.weekly_specials} />
+            </div>
+            <div className='py-20 flex flex-col w-full justify-center items-center text-center overflow-hidden'>
+              <p className='text-2xl lg:text-3xl font-bold text-primary-blue'>Our Partners</p>
+              <div className='w-11/12 lg:w-5/6 my-8'>
+                <Carousel />
+              </div>
+            </div>
+            <div className='py-20 w-full' id='services'>
+              <Services />
+            </div>
+            <div className=' relative my-8 w-100 flex flex-col justify-center lg:justify-between items-center'>
+              <div className=' mb-4 flex flex-col justify-center items-center pt-9  text-center '>
+                <p className='text-slate-400 text-sm lg:text-lg my-3'>A WORD FROM </p>
+                <p className='text-primary-blue font-black text-2xl lg:text-4xl'>Our Customers</p>
+              </div>
+              <div className='testimonials w-100  flex flex-col items-center'>
+                <Testimonial />
+              </div>
+            </div>
+          </>
+        )}
       </main>
     </div>
   );
